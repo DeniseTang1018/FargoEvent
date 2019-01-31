9b06079982acc331bd81663f245cafceb724f4e3
DeniseTang1018 <denisetang0630@gmail.com>
2019-01-31 11:40:31 -0600

    Both fragments completed

---
+++ app/src/main/java/com/example/fargoevent/ListFrag.java
@@ -0,0 +1,53 @@
+package com.example.fargoevent;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFrag extends android.support.v4.app.ListFragment {
+
+
+    private eventsListener eListener;
+    public ListFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.events)));
+        eListener.onEventsSelected(0);
+    }
+    public interface eventsListener{
+        public void onEventsSelected(int index);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            eListener = (eventsListener)context;
+
+        }catch (ClassCastException e){
+            throw new ClassCastException(context.toString()+ "must implement the interface called eventListener");
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        eListener.onEventsSelected(position);
+    }
+}
--- app/src/main/java/com/example/fargoevent/MainActivity.java
+++ app/src/main/java/com/example/fargoevent/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.fargoevent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListFrag.eventsListener{
 
+    private TextView tVDetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tVDetail = (TextView)findViewById(R.id.tVDetails);
+
+    }
+
+    @Override
+    public void onEventsSelected(int index) {
+        String [] details = getResources().getStringArray(R.array.details);
+        tVDetail.setText(details[index]);
+
     }
 }
